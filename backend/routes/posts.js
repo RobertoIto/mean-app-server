9342c5a98c91ea390b64519ac62ebbf9f13da41f
Rito <rito@my.centennialcollege.ca>
2019-04-17 20:10:39 -0400

    Lesson 92 - check app
    Paginator

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -28,14 +28,25 @@ const storage = multer.diskStorage({
 });
 
 router.get("", (req, res, next) => {
-    Post.find()
-        .then(documents => {
-            //console.log(documents);
-            res.status(200).json({
-                message: 'Post fetched successfully!',
-                posts: documents
-            });
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    let fetchedPosts;
+    if (pageSize && currentPage) {
+        postQuery
+            .skip(pageSize * (currentPage - 1))
+            .limit(pageSize);
+    }
+    postQuery.then(documents => {
+        fetchedPosts = documents;
+        return Post.count();
+    }).then(count => {
+        res.status(200).json({
+            message: 'Post fetched successfully!',
+            posts: fetchedPosts,
+            maxPosts: count
         });
+    });
 });
 
 
